e330ab3107c098d121f146b26e47241f6f945a2c
gabrielzv1233 <26gvurciaga@crschools.us>
2024-02-25 16:27:01 -0600

    gets movies from nites.is movies only work, no support for shows, unstested basic handeling if a movie cannot be played

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import re
 
 app = Flask(__name__)
 
@@ -32,12 +33,28 @@ def search_movies():
 def movie(tmdb_movie_id):
     api_key = '04aae18c13755d9ce23441e1221b3529'
     url = f"https://api.themoviedb.org/3/movie/{tmdb_movie_id}?api_key={api_key}"
-    
+
     response = requests.get(url)
     movie_data = response.json()
+
+    return render_template("movie.html", iframe=get_embed(movie_data['title'].lower()), title=movie_data['title'])
     
-    print(tmdb_movie_id)
-    return render_template('about_movie.html', movie_data=movie_data)
-    
+def get_embed(title):
+    filteredtitle = title.replace(" ", "-")
+    url = f"https://w1.nites.is/movies/{filteredtitle}/"
+    response = requests.get(url)
+    html_content = response.text
+
+    match = re.search('<link itemprop="embedUrl" href="(.*?)">', html_content)
+    if match:
+        movie_url = match.group(1)
+        movie_url = movie_url.replace(r"#038", r"amp")
+        movie_url = movie_url.replace(r"&trtype=1", r"&amp;trtype=1")
+        iframe = f'<iframe class="movie_iframe" frameborder="0" allowfullscreen="" data-lazy-src="{movie_url}" data-lazy-method="viewport" data-lazy-attributes="src" src="{movie_url}" data-gtm-yt-inspected-6="true"></iframe>'
+        return iframe
+    else:
+        print("No movie URL found.")
+        return f"original title{title} filtered {filteredtitle} request url {url}"
+
 if __name__ == '__main__':
     app.run(debug=True)
